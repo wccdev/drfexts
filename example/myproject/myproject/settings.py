14d3c52500a9cce5d992f77a08cde6f110400399
aiden <aiden_lu@wochacha.com>
2022-01-26 11:20:28 +0800

    Init

--- example/myproject/myproject/settings.py
+++ example/myproject/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*zc(n(krns9!(*pag20##rc94ci7w8dkdu!pvm7w%%!548%7e#'
+SECRET_KEY = 'django-insecure-*zc(n(krns9!(*pag20##rc94ci7w8dkdu!pvm7w%%!548%7e#'  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
