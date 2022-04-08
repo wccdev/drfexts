6c459319d97b389240b5664f9d1d326a1ba658ee
aiden_lu <aiden_lu@wochacha.com>
2022-04-08 14:59:14 +0800

    Bump version to 0.8.14.

--- drfexts/pagination.py
+++ drfexts/pagination.py
@@ -13,6 +13,13 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size_query_param = "page_size"
     max_page_size = 500
 
+    def paginate_queryset(self, queryset, request, view=None):
+        page_num = request.query_params.get(self.page_query_param)
+        # 判断，如果 page 为all 则取消分页返回所有
+        if page_num == 'all':
+            return None
+        return super().paginate_queryset(queryset, request, view)
+
     def get_paginated_response(self, data):
         return Response(
             {
