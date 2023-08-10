6d9c7d2d963e12a2eb7f505ce58a5639955141c8
aidenlu <aiden_lu@wochacha.com>
2023-08-10 19:06:45 +0800

    fix: fix bugs when initializing ChoiceArrayField

--- drfexts/fields.py
+++ drfexts/fields.py
@@ -403,29 +403,34 @@ class GenericRelation(ct_fields.GenericRelation):
         )
 
 
+class _TypedMultipleChoiceField(forms.TypedMultipleChoiceField):
+    def __init__(self, *args, **kwargs):
+        kwargs.pop("base_field", None)
+        kwargs.pop("max_length", None)
+        super().__init__(*args, **kwargs)
+
+
 class ChoiceArrayField(ArrayField):
     """
     A field that allows us to store an array of choices.
 
     Uses Django 4.2's postgres ArrayField
-    and a MultipleChoiceField for its formfield.
+    and a TypeMultipleChoiceField for its formfield.
 
     Usage:
 
-        choices = ChoiceArrayField(models.CharField(
-            max_length=...,
-            choices=(...,)),
-            default=[...]
+        choices = ChoiceArrayField(
+            models.CharField(max_length=..., choices=(...,)), blank=[...], default=[...]
         )
     """
 
     def formfield(self, **kwargs):
         defaults = {
-            "form_class": forms.MultipleChoiceField,
+            "form_class": _TypedMultipleChoiceField,
             "choices": self.base_field.choices,
+            "coerce": self.base_field.to_python,
         }
         defaults.update(kwargs)
-        # Skip our parent's formfield implementation completely as we don't
-        # care for it.
+        # Skip our parent's formfield implementation completely as we don't care for it.
         # pylint:disable=bad-super-call
         return super().formfield(**defaults)
