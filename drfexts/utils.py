2fd7fc010fdcff817ea7583b0cf73bbfd1c8f1fd
aidenlu <aiden_lu@wochacha.com>
2023-09-21 10:16:37 +0800

    fix(exception): get error message from ErrorDetail

--- drfexts/utils.py
+++ drfexts/utils.py
@@ -114,10 +114,10 @@ def get_error_msg(data: dict | list, default_field_key):
             errors.append(f"{k}: {'/n'.join(str(s) for s in v)}")
 
         return "/n".join(errors)
-    elif isinstance(data, exceptions.ErrorDetail):
+    elif isinstance(data, exceptions.exceptions):
         return str(data)
     elif isinstance(data, list):
         data = data[0]
-        return get_error_msg(data)
+        return get_error_msg(data, default_field_key)
 
     return str(data)
